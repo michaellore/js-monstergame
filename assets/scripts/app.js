747bd346380cab1446eb4e4fcae1de88f78e31ec
Michael Lore <mlore95@gmail.com>
2020-10-03 14:34:15 -0400

    add bonus life

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,12 +5,23 @@ const HEAL_VALUE = 15;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound(){
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+
+    if (currentPlayerHealth <=0 && hasBonusLife){
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert('Bonus Life saved you!');
+        
+    }
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You won!');
